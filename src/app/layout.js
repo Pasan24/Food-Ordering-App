e086bb4bb0985e1aac72df9ba9595659b04913e4
Pasan24 <pramudithapasan076@gmail.com>
2025-03-22 11:34:37 +0530

    Initial commit

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,14 +1,9 @@
-import { Geist, Geist_Mono } from "next/font/google";
+
+import { Roboto } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+const roboto = Roboto({
+  subsets: ["latin"], weights: [400, 500, 700] 
 });
 
 export const metadata = {
@@ -20,9 +15,10 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+        className={roboto.className}>
+          <main className="max-w-6xl mx-auto border">
         {children}
+        </main>
       </body>
     </html>
   );
