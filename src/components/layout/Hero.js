0e98bd2ab80291cac3c7b31cae56312aa9b9fd6d
Pasan24 <pramudithapasan076@gmail.com>
2025-03-24 08:37:07 +0530

    import right arrow icon for buttons

--- src/components/layout/Hero.js
+++ src/components/layout/Hero.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Image from 'next/image'
+import Right from '../icons/Right'
 
 export default function Hero() {
   return (
@@ -12,9 +13,9 @@ export default function Hero() {
         <h1 className='text-4xl font-semibold'>EveryThing is better with a Pizza </h1>
         <p className='mt-4'>Pizza is the missing piece that makes every day complete, a simple yet delecious joy in life.</p>
 
-        <div className="flex gap-4">
-            <button className="text-white bg-red-600  px-8 py-2 rounded-full">Order Now</button>
-            <button className="">Learn More</button>
+        <div className="flex gap-4 mt-4">
+            <button className="text-white bg-red-600  px-4 py-2 rounded-full flex gap-2 uppercase ">Order Now <Right/></button>
+            <button className="flex gap-2   py-2 text-gray-600 font-semibold ">Learn More <Right/></button>
         </div>
         </div>
 
