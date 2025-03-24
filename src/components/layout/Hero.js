196746054ea649b87c5ee5abc3f59f254536e064
Pasan24 <pramudithapasan076@gmail.com>
2025-03-24 09:09:29 +0530

    created HomeMenu file and do some codings

--- src/components/layout/Hero.js
+++ src/components/layout/Hero.js
@@ -5,16 +5,19 @@ import Right from '../icons/Right'
 export default function Hero() {
   return (
 
-    <section className='grid grid-cols-2'>
+    <section className='hero'>
         
 
-        <div>
+        <div className='py-12'>
 
-        <h1 className='text-4xl font-semibold'>EveryThing is better with a Pizza </h1>
-        <p className='mt-4'>Pizza is the missing piece that makes every day complete, a simple yet delecious joy in life.</p>
+        <h1 className='text-4xl font-semibold '>
+            EveryThing<br/> is better <br/> with a&nbsp;  <span className='text-red-500'> Pizza </span>
+         </h1>
+
+        <p className='mt-4 text-sm'>Pizza is the missing piece that makes every day complete, a simple yet delecious joy in life.</p>
 
         <div className="flex gap-4 mt-4">
-            <button className="text-white bg-red-600  px-4 py-2 rounded-full flex gap-2 uppercase ">Order Now <Right/></button>
+            <button className="text-white bg-red-600  px-4 py-2 rounded-full flex gap-2 uppercase text-sm items-center ">Order Now <Right/></button>
             <button className="flex gap-2   py-2 text-gray-600 font-semibold ">Learn More <Right/></button>
         </div>
         </div>
---
+++ src/components/layout/HomeMenu.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export default function HomeMenu() {
+  return (
+    <section>
+
+        <div>
+
+            
+        </div>
+
+        <div className='text-center'>
+        <h3 className='uppercase text-gray-600 font-semibold'> Check Out</h3>
+        <h3 className='text-red-500 font-bold text-4xl italic '> Menu</h3>
+        </div>
+
+   </section>
+  )
+}
