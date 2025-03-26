17f20fbd477190e67b60b5c540d6f4e2ed49f02a
Pasan24 <pramudithapasan076@gmail.com>
2025-03-26 23:54:02 +0530

    changed the styles again

--- src/components/layout/HomeMenu.js
+++ src/components/layout/HomeMenu.js
@@ -3,18 +3,18 @@ import Image from 'next/image';
 
 export default function HomeMenu() {
   return (
-    <section className='relative flex flex-col items-center'>
+    <section className=''>
       {/* Container for the salad images */}
-      <div className='relative flex justify-center items-center w-full h-60'>
+      <div className='absolute h-full left-0 right-0 w-full justify-start'>
         
         {/* Left Salad Image */}
-        <div className='h-40 w-40 absolute left-0 md:-left-12 lg:-left-20'>
-          <Image src={'/sallad1.png'} layout='intrinsic' width={160} height={160} objectFit='contain' alt='Salad 1' />
+        <div className='absolute left-8 -top-{70px} text-left -z-10'>
+          <Image src={'/sallad1.png'} width={109} height={189} alt='Salad' />
         </div>
         
         {/* Right Salad Image */}
-        <div className='h-40 w-40 absolute right-0 md:-right-12 lg:-right-20'>
-          <Image src={'/sallad2.png'} layout='intrinsic' width={160} height={160} objectFit='contain' alt='Salad 2' />
+        <div className='absolute -top-{100px} right-0 -z-10'>
+          <Image src={'/sallad2.png'} width={107} height={195} alt='Salad' />
         </div>
       </div>
 
